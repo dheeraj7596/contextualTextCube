f06ddc7819ac8d71f9a6a3bff8fea8c02d5243a2
dheeraj7596 <dheeraj7596.dm@gmail.com>
2019-10-27 19:08:59 -0700

    Added bert and clustering code.

---
+++ main.py
@@ -0,0 +1,59 @@
+from flair.embeddings import BertEmbeddings
+from flair.data import Sentence
+from sklearn.cluster import KMeans
+from nltk import sent_tokenize
+import pickle
+
+
+def get_sentences(path):
+    f = open(path, "r")
+    lines = f.readlines()
+    sentences = []
+    for line in lines:
+        line = line.strip()
+        sentences.extend(sent_tokenize(line))
+    f.close()
+    return sentences
+
+
+def print_results(km, sentences, mapping):
+    labels = km.labels_
+    for i, label in enumerate(labels):
+        sentence = sentences[mapping[i]["sentence"]]
+        sent = Sentence(sentence)
+        token_ind = mapping[i]["token"]
+        print("Sentence: ", sent)
+        print("Word: ", sent.tokens[token_ind])
+        print("Substring: ", sent.tokens[token_ind - 2:token_ind + 2])
+        print("label: ", label)
+        print("*" * 20)
+    print(km.labels_)
+
+
+if __name__ == "__main__":
+    embedding = BertEmbeddings('bert-base-uncased')
+    basepath = "./data/"
+    dataset = "nyt/"
+    dump_dir = basepath + dataset
+    path = basepath + dataset + "dataset.txt"
+    pkl_dump_dir = basepath + dataset
+
+    sentences = get_sentences(path)
+    km = KMeans(n_clusters=10)
+    tok_vecs = []
+    mapping = {}
+    i = 0
+
+    for sentence_ind, sent in enumerate(sentences):
+        sentence = Sentence(sent)
+        embedding.embed(sentence)
+        for token_ind, token in enumerate(sentence):
+            tok_vecs.append(token.embedding.detach().numpy())
+            mapping[i] = {"token": token_ind, "sentence": sentence_ind}
+            i += 1
+
+    print("Fitting KMeans..")
+    km.fit(tok_vecs)
+
+    pickle.dump(km, open(pkl_dump_dir + "km.pkl", "wb"))
+    pickle.dump(mapping, open(pkl_dump_dir + "mapping.pkl", "wb"))
