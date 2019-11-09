e887a4b206327e81453aba2a7d4cc075ecb28056
dheeraj7596 <dheeraj7596.dm@gmail.com>
2019-11-09 10:31:37 -0800

    Minor change

--- cluster.py
+++ cluster.py
@@ -30,12 +30,12 @@ def cluster_all_embeddings(word_dump_dir, cluster_dump_dir, threshold=0.7):
             if len(tok_vecs) == 0:
                 continue
 
-            km = KMeans(n_clusters=num_clusters, n_jobs=-1)
             word_cluster_dump_dir = cluster_dump_dir + word
             os.makedirs(word_cluster_dump_dir, exist_ok=True)
             if len(tok_vecs) < num_clusters:
                 pickle.dump(tok_vecs, open(word_cluster_dump_dir + "/cc.pkl", "wb"))
             else:
+                km = KMeans(n_clusters=num_clusters, n_jobs=-1)
                 km.fit(tok_vecs)
                 cc = km.cluster_centers_
                 sim = cosine_similarity(cc[0].reshape(1, -1), cc[1].reshape(1, -1))[0][0]
