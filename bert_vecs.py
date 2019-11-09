fd8d17d5b84c222b1a17504c47845ebfd7afd9e4
dheeraj7596 <dheeraj7596.dm@gmail.com>
2019-11-08 21:00:14 -0800

    bertvecs dump code added.

--- bert_vecs.py
+++ bert_vecs.py
@@ -1,28 +1,25 @@
 from flair.embeddings import BertEmbeddings
 from flair.data import Sentence
-from Word import Word
-from analyse import get_word_context
 from nltk import sent_tokenize
+from nltk.corpus import stopwords
 import pickle
 import flair, torch
+import os
 
 flair.device = torch.device('cuda:1')
 
 
-def get_all_embeddings(df, embedding):
+def get_all_embeddings(df, embedding, pkl_dump_dir):
+    stop_words = set(stopwords.words('english'))
+    stop_words.add("would")
     except_counter = 0
-    word_obj_list = []
-    word_sent_token_dict = {}
 
     for index, row in df.iterrows():
         if index % 100 == 0:
             print("Finished sentences: " + str(index) + " out of " + str(len(df)))
-        word_sent_token_dict[index] = {}
         line = row["sentence"]
-        label = row["label"]
         sentences = sent_tokenize(line)
         for sentence_ind, sent in enumerate(sentences):
-            word_sent_token_dict[index][sentence_ind] = {}
             sentence = Sentence(sent)
             try:
                 embedding.embed(sentence)
@@ -31,29 +28,27 @@ def get_all_embeddings(df, embedding):
                 print("Exception Counter: ", except_counter, sentence_ind, index, e)
                 continue
             for token_ind, token in enumerate(sentence):
+                word = token.text
+                if word in stop_words:
+                    continue
+                fname = pkl_dump_dir + word + "_.pkl"
                 vec = token.embedding.cpu().numpy()
-                word_obj = Word(name=token.text,
-                                context=get_word_context(token_ind, sent),
-                                tok_vec=vec,
-                                label=label
-                                )
-                word_sent_token_dict[index][sentence_ind][token_ind] = word_obj
-                word_obj_list.append(word_obj)
-    return word_obj_list, word_sent_token_dict
+                if os.path.isfile(fname):
+                    bert_vecs = pickle.load(open(fname, "rb"))
+                else:
+                    bert_vecs = []
+                if len(bert_vecs) > 10000:
+                    continue
+                bert_vecs.append(vec)
+                pickle.dump(bert_vecs, open(fname, "wb"))
 
 
 if __name__ == "__main__":
-    import sys
-
     embedding = BertEmbeddings('bert-base-uncased')
     basepath = "/data3/jingbo/dheeraj/"
     dataset = "nyt/"
     pkl_dump_dir = basepath + dataset
-    start = str(sys.argv[1])
-    end = str(sys.argv[2])
+    word_dump_dir = basepath + dataset + "wordvecs/"
 
     df = pickle.load(open(pkl_dump_dir + "/df_tokens_limit.pkl", "rb"))
-    word_obj_list, word_sent_token_dict = get_all_embeddings(df[int(start): int(end)], embedding)
-    pickle.dump(word_obj_list, open(pkl_dump_dir + "wordvecs/bert_vecs_" + str(start) + "_" + str(end) + ".pkl", "wb"))
-    pickle.dump(word_sent_token_dict,
-                open(pkl_dump_dir + "wordvecs/word_sent_token_dict_" + str(start) + "_" + str(end) + ".pkl", "wb"))
+    get_all_embeddings(df, embedding, word_dump_dir)
