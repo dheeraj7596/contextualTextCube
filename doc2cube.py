22d3a1bfc0a256b0379b422d6ad391e75850c23d
dheeraj7596 <dheeraj7596.dm@gmail.com>
2019-11-11 02:40:34 -0800

    iterations changed to 50

--- doc2cube.py
+++ doc2cube.py
@@ -168,7 +168,7 @@ if __name__ == "__main__":
     A_TD = get_ATD(df, word_to_index)
     U_D = np.transpose(A_TD)
     docfreq = get_doc_freq(df)
-    t = 200
+    t = 50
     threshold = 0.6
 
     for i in range(t):
