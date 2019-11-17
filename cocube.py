b507266183c34355d498e21a3fe083c69ea017fc
dheeraj7596 <dheeraj7596.dm@gmail.com>
2019-11-16 17:00:05 -0800

    Getting pred_labels from initial model

--- cocube.py
+++ cocube.py
@@ -6,6 +6,12 @@ import numpy as np
 import copy
 
 
+def get_label_term_json(pkl_dump_dir):
+    import json
+    dic = json.load(open(pkl_dump_dir + "seedwords.json", "r"))
+    return dic
+
+
 def create_index(word_vec):
     word_to_index = {}
     index_to_word = {}
@@ -16,68 +22,12 @@ def create_index(word_vec):
     return word_to_index, index_to_word
 
 
-def get_label_term_dict(labels, word_vec):
-    label_term_dict = defaultdict(set)
-    for i in labels:
-        if i == "stocks_and_bonds":
-            terms = ["stocks", "bonds$1"]
-            label_term_dict[i] = set(terms)
-        elif i == "the_affordable_care_act":
-            terms = ["health", "coverage", "medicaid"]
-            label_term_dict[i] = set(terms)
-        elif i == "gun_control":
-            terms = ["background$0", "firearms", "shooting$1"]
-            label_term_dict[i] = set(terms)
-        elif i == "federal_budget":
-            terms = ["tax", "debt"]
-            label_term_dict[i] = set(terms)
-        elif i == "energy_companies":
-            terms = ["energy", "solar", "gas"]
-            label_term_dict[i] = set(terms)
-        elif i == "immigration":
-            terms = ["citizenship", "immigrant", "illegal"]
-            label_term_dict[i] = set(terms)
-        elif i == "cosmos":
-            terms = ["space$0", "kepler", "planets"]
-            label_term_dict[i] = set(terms)
-        elif i == "gay_rights":
-            terms = ["gay rights"]
-            label_term_dict[i] = set(terms)
-        elif i == "business":
-            terms = ["sales", "battery", "airlines"]
-            label_term_dict[i] = set(terms)
-        elif i == "international_business":
-            terms = ["bank", "european", "euro"]
-            label_term_dict[i] = set(terms)
-        elif i == "law_enforcement":
-            terms = ["police", "judge$1", "prosecutors"]
-            label_term_dict[i] = set(terms)
-        elif i == "military":
-            terms = ["military", "marine", "army"]
-            label_term_dict[i] = set(terms)
-        elif i == "politics":
-            terms = ["democrats", "president$1", "senate"]
-            label_term_dict[i] = set(terms)
-        else:
-            terms = i.split("_")
-            for term in terms:
-                try:
-                    temp = word_vec[term]
-                    label_term_dict[i].add(term)
-                except:
-                    pass
-                try:
-                    temp = word_vec[term + "$0"]
-                    label_term_dict[i].add(term + "$0")
-                except:
-                    pass
-                try:
-                    temp = word_vec[term + "$1"]
-                    label_term_dict[i].add(term + "$1")
-                except:
-                    pass
-
-    return label_term_dict
+def print_label_term_dict(label_term_dict):
+    for label in label_term_dict:
+        print(label)
+        print("*" * 80)
+        for val in label_term_dict[label]:
+            print(val)
 
 
 def get_inv_doc_freq(df):
@@ -97,47 +47,17 @@ def get_inv_doc_freq(df):
     return docfreq
 
 
-def update_label_term_dict(df, label_term_dict, pred_labels, label_to_index, index_to_label, word_to_index,
-                           index_to_word, inv_docfreq, it):
+def get_label_docs_dict(df, label_term_dict, pred_labels):
     label_docs_dict = {}
-    label_count = len(label_to_index)
-    term_count = len(word_to_index)
-
-    E_LT = np.zeros((label_count, term_count))
-
     for l in label_term_dict:
         label_docs_dict[l] = []
-
     for index, row in df.iterrows():
         line = row["sentence"]
         label_docs_dict[pred_labels[index]].append(line)
+    return label_docs_dict
 
-    for l in label_docs_dict:
-        docs = label_docs_dict[l]
-        vect = CountVectorizer(vocabulary=list(word_to_index.keys()), tokenizer=lambda x: x.split())
-        X = vect.fit_transform(docs)
-        X_arr = X.toarray()
-        rel_freq = np.sum(X_arr, axis=0) / len(docs)
-        names = vect.get_feature_names()
-        for i, name in enumerate(names):
-            E_LT[label_to_index[l]][word_to_index[name]] = rel_freq[i]
 
-    for l in range(label_count):
-        zero_counter = 0
-        for t in range(term_count):
-            if E_LT[l][t] == 0:
-                continue
-            col_list = list(E_LT[:, t])
-            temp_list = copy.deepcopy(col_list)
-            temp_list.pop(l)
-            den = np.nanmax(temp_list)
-            if den == 0:
-                den = 0.0000000001
-                zero_counter += 1
-            temp = E_LT[l][t] / den
-            E_LT[l][t] = temp * inv_docfreq[index_to_word[t]]
-        print(index_to_label[l], zero_counter)
-
+def update(E_LT, index_to_label, index_to_word, it, label_count):
     word_map = {}
     for l in range(label_count):
         n = 10 * (it + 1)
@@ -157,18 +77,43 @@ def update_label_term_dict(df, label_term_dict, pred_labels, label_to_index, ind
     return label_term_dict
 
 
-def print_label_term_dict(label_term_dict):
-    for label in label_term_dict:
-        print(label)
-        print("*" * 80)
-        for val in label_term_dict[label]:
-            print(val)
+def update_label_term_dict(df, label_term_dict, pred_labels, label_to_index, index_to_label, word_to_index,
+                           index_to_word, inv_docfreq, it):
+    label_count = len(label_to_index)
+    term_count = len(word_to_index)
+    label_docs_dict = get_label_docs_dict(df, label_term_dict, pred_labels)
 
+    E_LT = np.zeros((label_count, term_count))
 
-def get_label_term_json(pkl_dump_dir):
-    import json
-    dic = json.load(open(pkl_dump_dir + "seedwords.json", "r"))
-    return dic
+    for l in label_docs_dict:
+        docs = label_docs_dict[l]
+        vect = CountVectorizer(vocabulary=list(word_to_index.keys()), tokenizer=lambda x: x.split())
+        X = vect.fit_transform(docs)
+        X_arr = X.toarray()
+        rel_freq = np.sum(X_arr, axis=0) / len(docs)
+        names = vect.get_feature_names()
+        for i, name in enumerate(names):
+            E_LT[label_to_index[l]][word_to_index[name]] = rel_freq[i]
+
+    for l in range(label_count):
+        zero_counter = 0
+        for t in range(term_count):
+            if E_LT[l][t] == 0:
+                continue
+            # col_list = list(E_LT[:, t])
+            # temp_list = copy.deepcopy(col_list)
+            # temp_list.pop(l)
+            # den = np.nanmax(temp_list)
+            # if den == 0:
+            #     den = 0.0000000001
+            #     zero_counter += 1
+            # temp = E_LT[l][t] / den
+            temp = E_LT[l][t]
+            E_LT[l][t] = temp * inv_docfreq[index_to_word[t]]
+        print(index_to_label[l], zero_counter)
+
+    label_term_dict = update(E_LT, index_to_label, index_to_word, it, label_count)
+    return label_term_dict
 
 
 if __name__ == "__main__":
@@ -181,8 +126,7 @@ if __name__ == "__main__":
 
     word_to_index, index_to_word = create_index(word_vec)
     labels, label_to_index, index_to_label = get_distinct_labels(df)
-    # label_term_dict = get_label_term_json(pkl_dump_dir)
-    label_term_dict = get_label_term_dict(labels, word_vec)
+    label_term_dict = get_label_term_json(pkl_dump_dir)
     inv_docfreq = get_inv_doc_freq(df)
 
     t = 10
@@ -191,6 +135,8 @@ if __name__ == "__main__":
         print("ITERATION ", i)
         print("Going to train classifier..")
         pred_labels = train_classifier(df, labels, label_term_dict, label_to_index, index_to_label)
+        if i == 0:
+            pickle.dump(pred_labels, open(pkl_dump_dir + "seedwords_pred.pkl", "wb"))
         print("Updating label term dict..")
         label_term_dict = update_label_term_dict(df, label_term_dict, pred_labels, label_to_index, index_to_label,
                                                  word_to_index, index_to_word, inv_docfreq, i)
