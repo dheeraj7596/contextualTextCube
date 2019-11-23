9a54b782488eef554b34dce3ca9952b1b5730b2d
dheeraj7596 <dheeraj7596.dm@gmail.com>
2019-11-23 08:37:35 -0800

    Minor fix

--- cocube.py
+++ cocube.py
@@ -14,6 +14,7 @@ def get_popular_matrix(index_to_word, docfreq, inv_docfreq, label_count, label_d
     E_LT = np.zeros((label_count, term_count))
     components = {}
     for l in label_docs_dict:
+        components[l] = {}
         docs = label_docs_dict[l]
         docfreq_local = calculate_doc_freq(docs)
         vect = CountVectorizer(vocabulary=list(word_to_index.keys()), tokenizer=lambda x: x.split())
