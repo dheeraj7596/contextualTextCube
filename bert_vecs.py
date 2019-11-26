a5c2a135089ed9d30665e511a15da8fbdcdb7478
dheeraj7596 <dheeraj7596.dm@gmail.com>
2019-11-26 08:53:42 -0800

    changed the dump dir

--- bert_vecs.py
+++ bert_vecs.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     basepath = "/data3/jingbo/dheeraj/"
     dataset = "arxiv/"
     pkl_dump_dir = basepath + dataset
-    word_dump_dir = basepath + dataset + "wordvecs_tokenized_fresh/"
+    word_dump_dir = basepath + dataset + "wordvecs_tokenized_fresh_uncap/"
 
     df = pickle.load(open(pkl_dump_dir + "/df_tokens_limit.pkl", "rb"))
     get_all_embeddings(df, embedding, word_dump_dir)
